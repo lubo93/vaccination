981e880ff48d7230e7d463ea1bee9c9d12c961a3
lubo93 <lucasb@ethz.ch>
2021-01-18 10:00:01 +0100

    added DOPRI solver

--- model/lib/simulation.py
+++ model/lib/simulation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.integrate import solve_ivp
 
 # epidemic model class
 class epidemic_model:   
@@ -110,8 +111,8 @@ class epidemic_model:
         
         # total vaccination count
         self.vaccine_total = 0
-        
-    def step(self):
+    
+    def step_EULER(self):
         """
         Euler forward integration step of SEIRD dynamics with prime and
         prime-boost vaccination.
@@ -197,6 +198,88 @@ class epidemic_model:
         self.Ipp = Ipp_new
         self.R = R_new
         self.D = D_new
+        
+    def step_DOPRI(self):
+        """
+        Dormand--Prince integration step of SEIRD dynamics with prime and
+        prime-boost vaccination.
+        """
+                        
+        delta_S = lambda y: -self.beta*y[0]*y[6] -  \
+        self.betap*y[0]*y[7] - self.betapp*y[0]*y[8] - \
+        self.nu_1*np.heaviside(y[0]-self.nu_1,0) + self.eta_1*y[1] + \
+        self.eta_2*y[2]
+        
+        delta_Sp = lambda y: self.nu_1*np.heaviside(y[0]-self.nu_1,0) - self.beta_1*y[1]*y[6] -  \
+        self.beta_1p*y[1]*y[7] - self.beta_1pp*y[1]*y[8] - \
+        self.nu_2*np.heaviside(y[1]-self.nu_2,0)*np.heaviside(self.t-self.td,0) \
+        - self.eta_1*y[1]
+        
+        delta_Spp = lambda y: self.nu_2*np.heaviside(y[1]-self.nu_2,0)*np.heaviside(self.t-self.td,0) - \
+        self.beta_2*y[2]*y[6] - self.beta_2p*y[2]*y[7] - \
+        self.beta_2pp*y[2]*y[8] - self.eta_2*y[2]
+        
+        delta_E = lambda y: self.beta*y[0]*y[6] + self.betap*y[0]*y[7] + \
+        self.betapp*y[0]*y[8] - self.sigma*y[3]
+        
+        delta_Ep = lambda y: self.beta_1*y[1]*y[6] + self.beta_1p*y[1]*y[7] + \
+        self.beta_1pp*y[1]*y[8] - self.sigma_1*y[4]
+        
+        delta_Epp = lambda y: self.beta_2*y[2]*y[6] + \
+        self.beta_2p*y[2]*y[7] + self.beta_2pp*y[2]*y[8] - \
+        self.sigma_2*y[5]
+        
+        delta_I = lambda y: self.sigma*y[3] - self.gamma*y[6]
+        
+        delta_Ip = lambda y: self.sigma_1*y[4] - self.gammap*y[7]
+        
+        delta_Ipp = lambda y: self.sigma_2*y[5] - self.gammapp*y[8]
+        
+        delta_R = lambda y: self.gamma*(1-self.IFR)*y[6] + \
+        self.gammap*(1-self.IFRp)*y[7] + \
+        self.gammapp*(1-self.IFRpp)*y[8]
+        
+        delta_D = lambda y: self.gamma*self.IFR*y[6] + \
+        self.gammap*self.IFRp*y[7] + \
+        self.gammapp*self.IFRpp*y[8]
+        
+        delta_vaccine = lambda y: self.nu_1*np.heaviside(y[0]-self.nu_1,0) \
+        +self.nu_2*np.heaviside(y[1]-self.nu_2,0)*np.heaviside(self.t-self.td,0)        
+
+        
+        f = lambda t, y: [delta_S(y), delta_Sp(y), delta_Spp(y), 
+                          delta_E(y), delta_Ep(y), delta_Epp(y),
+                          delta_I(y), delta_Ip(y), delta_Ipp(y),
+                          delta_R(y), delta_D(y), delta_vaccine(y)]
+        
+        y0 = [self.S, self.Sp, self.Spp, self.E, self.Ep, self.Epp, \
+              self.I, self.Ip, self.Ipp, self.R, self.D, self.vaccine_total]
+        
+        sol = solve_ivp(f, 
+                        [0,self.dt], 
+                        y0,
+                        method='RK45', 
+                        t_eval=[self.dt])
+             
+       
+        # determine maximum rate of change in the number of deaths
+        if sol.y[10][0]-self.D > self.delta_d:
+            self.delta_d = sol.y[10][0]-self.D
+        
+        # update current compartment values
+        self.S =  sol.y[0][0]
+        self.Sp =  sol.y[1][0]
+        self.Spp =  sol.y[2][0]
+        self.E =  sol.y[3][0]
+        self.Ep =  sol.y[4][0]
+        self.Epp =  sol.y[5][0]
+        self.I =  sol.y[6][0]
+        self.Ip =  sol.y[7][0]
+        self.Ipp =  sol.y[8][0]
+        self.R =  sol.y[9][0]
+        self.D =  sol.y[10][0]
+        
+        self.vaccine_total = sol.y[11]
 
     def simulate(self):
         """
@@ -205,7 +288,7 @@ class epidemic_model:
         """
         
         for i in range(self.N):
-            self.step()
+            self.step_DOPRI()
             self.t += self.dt
             self.S_arr.append(self.S)
             self.Sp_arr.append(self.Sp)
